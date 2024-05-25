5e8e4bcb3a25ff0bccdc2794940a4bcddddf7d00
trankien <kien8b2003@gmail.com>
2024-05-25 22:44:28 +0700

    add-quanlythietbi

--- GYM/demo6/src/main/resources/static/js/quanlythietbi.js
+++ GYM/demo6/src/main/resources/static/js/quanlythietbi.js
@@ -1,3 +1,74 @@
-const initData = [{
-  
-}]
+const initData = [
+  {
+    id: "001",
+    name: "Máy chạy bộ",
+    image: "https://i.pinimg.com/564x/0a/af/a0/0aafa0c1806462d5122d3156e440ff01.jpg",
+    type: "Cardio",
+    purchaseDate: "2024-04-01",
+    quantity: 5,
+    status: "Active"
+  },
+  {
+    id: "002",
+    name: "Tạ đơn",
+    image: "https://i.pinimg.com/564x/95/84/7f/95847fe8832d8859e386233fb993a44c.jpg",
+    type: "Tạ",
+    purchaseDate: "2024-04-02",
+    quantity: 20,
+    status: "Active"
+  },
+  {
+    id: "003",
+    name: "Xe đạp tập",
+    image: "https://i.pinimg.com/564x/de/3e/0e/de3e0e9a15003a4f76c65e25dcec6e05.jpg",
+    type: "Cardio",
+    purchaseDate: "2024-04-01",
+    quantity: 10,
+    status: "Active"
+  },
+  {
+    id: "004",
+    name: "Máy ép ngực",
+    image: "https://i.pinimg.com/564x/78/f8/b7/78f8b7661af9687b581e92089da2971b.jpg",
+    type: "Sức mạnh",
+    purchaseDate: "2024-04-01",
+    quantity: 7,
+    status: "Active"
+  },
+  {
+    id: "005",
+    name: "Máy kéo xô",
+    image: "https://i.pinimg.com/564x/06/8d/75/068d75712ff87a458e24cf082e7ee4d8.jpg",
+    type: "Sức mạnh",
+    purchaseDate: "2024-04-01",
+    quantity: 2,
+    status: "Maintaenance"
+  },
+  {
+    id: "006",
+    name: "Máy gập bụng",
+    image: "https://i.pinimg.com/564x/74/08/59/7408594e7fd92d03a23ce72de10904d9.jpg",
+    type: "Sức mạnh",
+    purchaseDate: "2024-04-01",
+    quantity: 9,
+    status: "Active"
+  },
+  {
+    id: "007",
+    name: "Máy tập chân",
+    image: "https://i.pinimg.com/564x/b2/e3/ad/b2e3adf2068f661014ffcfee1b280445.jpg",
+    type: "Sức mạnh",
+    purchaseDate: "2024-03-27",
+    quantity: 14,
+    status: "Maintaenance"
+  },
+  {
+    id: "008",
+    name: "Bóng tập thể lực",
+    image: "https://th.bing.com/th/id/OIP.xJ3Y0Pcpo7rTf_m7iTla-AHaHa?w=980&h=980&rs=1&pid=ImgDetMain",
+    type: "Cardio",
+    purchaseDate: "2024-02-19",
+    quantity: 25,
+    status: "Active"
+  }
+]

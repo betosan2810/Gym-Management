4bb35435f9939de30795af6bc8bc70a6c7886273
betosan2810 <vle80444@gmail.com>
2024-05-26 22:21:10 +0700

    fix image

--- GYM/demo6/src/main/java/com/example/demo/connection/DBConnection.java
+++ GYM/demo6/src/main/java/com/example/demo/connection/DBConnection.java
@@ -6,25 +6,20 @@ import java.sql.SQLException;
 
 public class DBConnection {
 
-
-    public static Connection getConnection()
-    {
+    public static Connection getConnection() {
         String url = "jdbc:mysql://localhost:3306/gym";
         String user = "root";
-        String password = "Kien1234@";
+        String password = "";
         Connection conn = null;
         try {
             Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
             conn = DriverManager.getConnection(url, user, password);
             return conn;
-        }catch (ClassNotFoundException e)
-        {
+        } catch (ClassNotFoundException e) {
 
-        }catch (SQLException throwables)
-        {
+        } catch (SQLException throwables) {
             throwables.printStackTrace();
-        }
-        finally {
+        } finally {
             return conn;
         }
     }
